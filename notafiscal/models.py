4ddef00d9900264c021cc1f625ea70e8556c2c36
Henrique Apocalypse <rick.apocalypse@hotmail.com>
2022-07-27 21:33:14 -0300

    implementacao do models da nota fiscal

--- notafiscal/models.py
+++ notafiscal/models.py
@@ -1,2 +1,24 @@
 from django.db import models
+from provider.models import Provider
 
+class NotaFiscal(models.Model):
+
+    STATUS = (
+        ('1', 'não pago'),
+        ('2', 'pago'),
+    )
+
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, null=True)
+    number = models.IntegerField()
+    data = models.DateField(auto_now=True)
+    name_product = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_valor = models.DecimalField(max_digits=10, decimal_places=2)
+    payment = models.CharField(max_length=1, choices=STATUS)
+
+    def __str__(self):
+        return self.name_product
+    class Meta:
+        verbose_name = 'Nota Fiscal'
+        verbose_name_plural = 'Notas Fiscais'
+        ordering = ['data']
--- provider/models.py
+++ provider/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Provider(models.Model):
     # provider_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255, null = True)
+    provider = models.CharField(max_length=255, null = True)
     cnpj = models.CharField(max_length=14, unique = True)
     telephone = models.CharField(max_length=14, null = True)
 
     def __str__(self):
-        return self.name
+        return self.provider

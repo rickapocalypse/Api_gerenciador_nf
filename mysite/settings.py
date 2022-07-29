1aad4f3acead5e2a65f4a3c0fce98b712e2cc041
Henrique Apocalypse <rick.apocalypse@hotmail.com>
2022-07-28 23:32:01 -0300

    criacao do home

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'mysite',
     'provider',
     'notafiscal',
+    'home',
 ]
 
 MIDDLEWARE = [
--- notafiscal/views.py
+++ notafiscal/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def NotaFiscalList(request):
+    return HttpResponse('NotaFiscalList')
+    # return render(request, 'notafiscal/notafiscal_list.html')

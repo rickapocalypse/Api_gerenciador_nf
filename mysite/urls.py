32def3f6346a4dad8e98c5f82a078f2d6b19f3a0
Henrique Apocalypse <rick.apocalypse@hotmail.com>
2022-07-30 18:16:17 -0300

    css do home finalizado

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('notafiscal/', include('notafiscal.urls')),
+    path('provider/', include('provider.urls')),
 ]
--- provider/views.py
+++ provider/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def providers(request):
+    return render(request, 'providers/providers.html')
